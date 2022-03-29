aab3e130c13013dd565a54f1dcb15737ba5b2aa4
Angel Tabares <taho2509@gmail.com>
2022-03-29 01:58:37 -0300

    Time: 1866 ms (5.06%), Space: 12.6 MB (91.66%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -19,18 +19,12 @@ public:
         
         for(int t = 1; t <= k; ++t) {
             int accum = 0;
-            for(int j = 0; j < piles[i].size() && j < k; ++j) {
+            for(int j = 0; j < piles[i].size() && (j < k || j + 1 < t); ++j) {
                 accum += piles[i][j];
 
-                DP[j + 1][i%2] = max(DP[j + 1][(i+1)%2], accum);
-            }
-        }
-        
-        for(int t = 1; t <= k; ++t) {
-            int accum = 0;
-            for(int j = 0; j < piles[i].size() && j + 1 < t; ++j) {
-                accum += piles[i][j];
-                DP[t][i%2] = max(max(DP[t][(i+1)%2],DP[t][i%2]), accum + DP[t - (j + 1)][(i+1)%2]);
+                if(j < k) DP[j + 1][i%2] = max(max(DP[j + 1][(i+1)%2],DP[j + 1][i%2]), accum);
+                
+                if(j + 1 < t) DP[t][i%2] = max(max(DP[t][(i+1)%2],DP[t][i%2]), accum + DP[t - (j + 1)][(i+1)%2]);
             }
         }
     }
