011c21a0efd4c1d0e49e5a71f57770cafc5cba2e
Angel Tabares <taho2509@gmail.com>
2022-03-29 02:04:57 -0300

    Time: 1724 ms (5.06%), Space: 12.4 MB (91.75%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -6,26 +6,21 @@ public:
         
         vector<vector<int>> DP(k + 1, vector<int>(2,0));
         
-        dfs(piles, k, 0, DP);
+        for(int i = n - 1; i >= 0; --i) {
+            for(int t = 1; t <= k; ++t) {
+                int accum = 0;
+                for(int j = 0; j < piles[i].size() && (j < k || j + 1 < t); ++j) {
+                    accum += piles[i][j];
+
+                    if(j < k) DP[j + 1][i%2] = max(max(DP[j + 1][(i+1)%2],DP[j + 1][i%2]), accum);
+
+                    if(j + 1 < t) DP[t][i%2] = max(max(DP[t][(i+1)%2],DP[t][i%2]), accum + DP[t - (j + 1)][(i+1)%2]);
+                }
+            }
+        } 
         
         return max(DP[k][0],DP[k][1]);
     }
     
-    void dfs(vector<vector<int>>& piles, int k, int i, vector<vector<int>> &DP) {
-        int n = piles.size();
-        if(i >= n) return;
-        
-        dfs(piles, k, i + 1, DP);
-        
-        for(int t = 1; t <= k; ++t) {
-            int accum = 0;
-            for(int j = 0; j < piles[i].size() && (j < k || j + 1 < t); ++j) {
-                accum += piles[i][j];
 
-                if(j < k) DP[j + 1][i%2] = max(max(DP[j + 1][(i+1)%2],DP[j + 1][i%2]), accum);
-                
-                if(j + 1 < t) DP[t][i%2] = max(max(DP[t][(i+1)%2],DP[t][i%2]), accum + DP[t - (j + 1)][(i+1)%2]);
-            }
-        }
-    }
 };
