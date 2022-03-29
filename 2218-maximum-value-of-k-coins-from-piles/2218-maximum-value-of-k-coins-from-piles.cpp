92207751e31ca0b02324655634996af9295f8e3a
Angel Tabares <taho2509@gmail.com>
2022-03-29 01:56:35 -0300

    Time: 1882 ms (5.06%), Space: 12.6 MB (91.66%) - LeetHub

---
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -0,0 +1,37 @@
+#define big 300000000
+class Solution {
+public:
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        int n = piles.size();
+        
+        vector<vector<int>> DP(k + 1, vector<int>(2,0));
+        
+        dfs(piles, k, 0, DP);
+        
+        return max(DP[k][0],DP[k][1]);
+    }
+    
+    void dfs(vector<vector<int>>& piles, int k, int i, vector<vector<int>> &DP) {
+        int n = piles.size();
+        if(i >= n) return;
+        
+        dfs(piles, k, i + 1, DP);
+        
+        for(int t = 1; t <= k; ++t) {
+            int accum = 0;
+            for(int j = 0; j < piles[i].size() && j < k; ++j) {
+                accum += piles[i][j];
+
+                DP[j + 1][i%2] = max(DP[j + 1][(i+1)%2], accum);
+            }
+        }
+        
+        for(int t = 1; t <= k; ++t) {
+            int accum = 0;
+            for(int j = 0; j < piles[i].size() && j + 1 < t; ++j) {
+                accum += piles[i][j];
+                DP[t][i%2] = max(max(DP[t][(i+1)%2],DP[t][i%2]), accum + DP[t - (j + 1)][(i+1)%2]);
+            }
+        }
+    }
+};
