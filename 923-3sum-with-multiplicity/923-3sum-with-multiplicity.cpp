df816d7bec9f870f6e22b30e9e7731a825c2a0ee
Angel Tabares <taho2509@gmail.com>
2022-04-05 22:07:42 -0400

    Time: 428 ms (45.92%), Space: 10.4 MB (90.82%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    int threeSumMulti(vector<int>& arr, int target) {
-        vector<int> S(301, 0);
-        vector<int> D(301, 0);
-        vector<int> T(301, 0);
-        int n = arr.size();
+    int threeSumMulti(vector<int>& nums, int target) {
+        int n = nums.size();
+        int ans = 0;
+        sort(nums.begin(), nums.end());
         
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j <= target; ++j) {
-                if(i >= 2) {
-                    if(j - arr[i] >= 0) {
-                        T[j] += D[j - arr[i]];
-                        T[j] %= 1000000007;
+        for(int i = 0; i < n; ++i) {            
+            int j = i + 1, k = n - 1;
+            while(j < k) {
+                if(nums[i] + nums[j] + nums[k] == target) {
+                    if(nums[j] == nums[k]) {
+                        ans += ((k - j)*(k - j + 1))/2;
+                        ans %= 1000000007;
+                        break;
                     }
+                    int left = 1, right = 1;
+                    while(j + 1 < k && nums[j+1] == nums[j]) {
+                        ++left;
+                        ++j;
+                    }
+                    while(j + 1 < k && nums[k - 1] == nums[k]) {
+                        ++right;
+                        --k;
+                    }
+                    ans += left*right;
+                    ans %= 1000000007;
+                    ++j; --k;
+                } else if(nums[i] + nums[j] + nums[k] > target) {
+                    --k;
+                } else {
+                    ++j;
                 }
             }
-            for(int j = 0; j <= target; ++j) {
-                if(i >= 1) {
-                    if(j - arr[i] >= 0) {
-                        D[j] += S[j - arr[i]];
-                        D[j] %= 1000000007;
-                    }
-                }
-            }
-            S[arr[i]]++;
         }
         
-        return T[target];
+        return ans;
     }
 };
