d854d40faaf9dcf6c7c701ca76fd230408c00c16
Angel Tabares <taho2509@gmail.com>
2022-04-12 20:56:25 -0400

    Time: 0 ms (100.00%), Space: 6.8 MB (18.13%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> matrix(n, vector<int>(n,0));
+        int counter = 1;
+        vector<int> x = {0,1,0,-1};
+        vector<int> y = {1,0,-1,0};
+        int currentDir = 0;
+        queue<pair<int,int>> next;
+        next.push({0,0});
+        int nextx, nexty;
+        
+        while(true) {
+            auto current = next.front();
+            next.pop();
+            
+            matrix[current.first][current.second] = counter;
+            if(counter == n*n) break;
+            
+            counter++;
+            
+            nextx = current.first + x[currentDir % 4];
+            nexty = current.second + y[currentDir % 4];
+            
+            // change of dir
+            if(nextx >= n || nextx < 0 || nexty >= n || nexty < 0 || matrix[nextx][nexty] != 0) {
+                currentDir++;
+                nextx = current.first + x[currentDir % 4];
+                nexty = current.second + y[currentDir % 4];
+            }
+            
+            next.push({nextx, nexty});
+        }
+        
+        return matrix;
+    }
+};
