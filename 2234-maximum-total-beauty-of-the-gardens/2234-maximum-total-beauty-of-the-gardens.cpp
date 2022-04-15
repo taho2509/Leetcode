5933d3e6ffd21c0694c827c7b8bee9b635da994a
Angel Tabares <taho2509@gmail.com>
2022-04-15 18:01:56 -0400

    Time: 274 ms (78.05%), Space: 85.3 MB (57.95%) - LeetHub

---
+++ 2234-maximum-total-beauty-of-the-gardens/2234-maximum-total-beauty-of-the-gardens.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    long long maximumBeauty(vector<int>& flowers, long long newFlowers, int target, int full, int partial) {
+        //cout << "start" << endl;
+        sort(flowers.begin(), flowers.end());
+        
+        flowers[0] = min(flowers[0], target);
+        
+        int n = flowers.size();
+        vector<long long> cost(n, 0);
+        
+        for(int i = 1; i < n; ++i) {
+            flowers[i] = min(flowers[i], target);
+            cost[i] = cost[i - 1];
+            cost[i] += (flowers[i] - (long long)flowers[i - 1])*i;
+        }
+        
+        if(flowers[0] == target) return (long long)n*full;
+        
+        int j = n - 1;
+        while(j >= 0 && flowers[j] == target) --j;
+        
+        if(newFlowers >= cost[n - 1] + (target - flowers[n - 1]) * n)
+            return max((long long)n*full, (long long)full*(n - 1) + (long long)partial*(target-1));
+        
+        //cout << "full" << endl;
+        
+        long long ans = 0;
+        int idx;
+        long long bar;
+        while(newFlowers >= 0) {
+            auto it = lower_bound(cost.begin(), cost.begin() + j, newFlowers);
+            idx = it - cost.begin();
+
+            if(cost[idx] > newFlowers){
+                --idx;
+                //cout << "change ";
+            }
+            //cout << idx << endl;
+            
+            bar = flowers[idx] + (newFlowers - cost[idx])/(idx + 1);
+            
+            ans = max(ans, (long long)full*(n - j - 1) + partial*(bar));
+            
+            newFlowers -= (target - flowers[j]);
+            --j;
+        }
+        
+        return ans;
+    }
+};
