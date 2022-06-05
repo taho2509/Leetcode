c7de698f31d1e5c0782b534922f0ba89056de243
Angel Tabares <taho2509@gmail.com>
2022-06-04 20:13:10 -0400

    Time: 14 ms (31.12%), Space: 8.5 MB (20.82%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+int ans;
+public:
+    int totalNQueens(int n) {
+        ans = 0;
+        for(int i = 0; i < n; ++i) create({}, {0, i}, n);
+        return ans;
+    }
+    
+    void create(vector<pair<int,int>> queens, pair<int,int> last, int n) {
+        queens.push_back(last);
+        if(queens.size() == n) { ++ans; return; }
+        
+        for(int i = 0; i < n; ++i) {
+            if(canPlaceIt(queens, i)) {
+                create(queens, {queens.size(), i}, n);
+            }
+        }
+    }
+    
+    bool canPlaceIt(vector<pair<int,int>> &queens, int i) {
+        int x2 = queens.size(), y2 = i;
+        
+        for(auto [x1, y1]: queens) {
+            if(x1 == x2 || y1 == y2) return false;
+            if(y1 - x1 == y2 - x2) return false;
+            if(y1 + x1 == y2 + x2) return false;
+        }
+        
+        return true;
+    }
+};
