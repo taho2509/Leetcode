fb18355e91d2d7726f5a0a4fdfa713ccbe6a2131
Angel Tabares <taho2509@gmail.com>
2022-06-14 21:32:43 -0400

    Time: 140 ms (51.39%), Space: 27.1 MB (21.64%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,50 +1,24 @@
 class Solution {
-vector<int> visited;
 public:
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(), words.end(), [](string a, string b) -> bool {return a.size() < b.size();});
-        visited = vector<int>(words.size(), -1);
-
+        sort(words.begin(), words.end(), [](string a, string b) -> bool {
+            return a.size() < b.size();
+        });
+        
+        unordered_map<string,int> large;
         int m = 0;
-        for(int i = 0; i < words.size(); ++i) {
-            m = max(m, 1 + check(words, i));
+        for(string s: words){
+            large[s] = 1;
+            
+            for(int i = 0; i < s.size(); ++i) {
+                string ss = s.substr(0,i) + s.substr(i + 1);
+                if(large.count(ss) != 0) {
+                    large[s] = max(large[s], 1 + large[ss]);
+                }
+            }
+            m = max(m, large[s]);
         }
         
         return m;
-    }
-    
-    int check(vector<string>& words, int i) {
-        if(i == words.size()) return 0;
-        if(visited[i] != -1) return visited[i];
-        int m = 0;
-        for(int j = i + 1; j < words.size(); ++j) {
-            if(words[i].size() + 1 < words[j].size()) break;
-            if(words[i].size() + 1 == words[j].size()) {
-                if(predecessor(words[i], words[j])) {
-                    m = max(m, 1 + check(words, j));
-                }
-            }
-        }
-        
-        visited[i] = m;
-
-        return m;
-    }
-    
-    bool predecessor(string &s1, string &s2) {
-        bool one = false;
-        for(int i = 0; i < s1.size(); ++i) {
-            if(one) {
-                if(s1[i] != s2[i + 1]) {
-                    return false;
-                }
-            } else {
-                if(s1[i] != s2[i]) {
-                    one = true;
-                    --i;
-                }
-            }
-        }
-        return true;
     }
 };
