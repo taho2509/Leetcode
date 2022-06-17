d61ce399cf416f72a33751cf68fb5b22c7d94d60
Angel Tabares <taho2509@gmail.com>
2022-06-16 21:59:28 -0400

    Time: 7 ms (96.05%), Space: 21.3 MB (55.06%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -10,63 +10,22 @@
  * };
  */
 class Solution {
-map<tuple<TreeNode*, bool, bool>, int> DP;
+    int ans = 0;
 public:
-    int minCameraCover(TreeNode* root) {
-        return min (find(root, true, false), find(root, false, false));
+    int minCameraCover(TreeNode* root) 
+    {
+        return (dfs(root) < 1 ? 1 : 0) + ans;
     }
-    
-    int find(TreeNode* r, bool cam, bool parentHaveCam) {
-        if(r->left == nullptr && r->right == nullptr) {
-            if(cam) return 1;
-            if(parentHaveCam) return 0;
-            return INT_MAX - 100;
+
+    int dfs(TreeNode* root) 
+    {
+        if (!root) return 2;
+        int left = dfs(root->left), right = dfs(root->right);
+        if (left == 0 || right == 0) 
+        {
+            ++ans;
+            return 1;
         }
-        
-        if(DP.find({r, cam, parentHaveCam}) != DP.end()) return DP[{r, cam, parentHaveCam}];
-        
-        int left = 0, right = 0;
-        int ans;
-        
-        if(cam) {
-            if(r->left != nullptr) {
-                left = min(find(r->left, false, cam), find(r->left, true, cam));
-            }
-            if(r->right != nullptr) {
-                right = min(find(r->right, false, cam), find(r->right, true, cam));
-            }
-            ans = 1 + left + right;
-        } else {
-            if(parentHaveCam) {
-                if(r->left != nullptr) {
-                    left = min(find(r->left, false, cam), find(r->left, true, cam));
-                }
-                if(r->right != nullptr) {
-                    right = min(find(r->right, false, cam), find(r->right, true, cam));
-                }
-                ans = left + right;
-            } else {
-                if(r->left == nullptr) {
-                    ans = find(r->right, true, cam);
-                } else if(r->right == nullptr) {
-                    ans = find(r->left, true, cam);
-                } else {
-                    ans = min(
-                        find(r->left, true, cam) 
-                        + min(
-                            find(r->right, true, cam),
-                            find(r->right, false, cam)
-                        ),
-                        find(r->right, true, cam) 
-                        + min(
-                            find(r->left, true, cam),
-                            find(r->left, false, cam)
-                        )
-                    );
-                }
-            }
-        }
-        DP[{r, cam, parentHaveCam}] = ans;
-        return ans;
+        return left == 1 || right == 1 ? 2 : 0;
     }
 };
