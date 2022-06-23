64bb82bc1c23884bd5f0fa090945e115bea48082
Angel Tabares <taho2509@gmail.com>
2022-06-22 22:15:05 -0400

    Time: 323 ms (62.30%), Space: 56.3 MB (58.52%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(), [](vector<int> &a, vector<int> &b) -> bool {
+            return a[1] < b[1];
+        });
+        
+        priority_queue<int> pq;
+        int time = 0;
+        
+        for(int i = 0; i < courses.size(); ++i) {
+            time += courses[i][0];
+            pq.push(courses[i][0]);
+            if(time > courses[i][1]) {
+                time -= pq.top();
+                pq.pop();
+            }
+        }
+        
+        return pq.size();
+    }
+};
