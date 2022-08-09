d56e4b240e83fe121e40f0028c2ac610045c0f23
Angel Tabares <taho2509@gmail.com>
2022-08-08 22:59:47 -0400

    Time: 4 ms (66.42%), Space: 7.1 MB (66.21%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,40 +1,10 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
+        int n1 = str1.size(), n2 = str2.size();
         
-        int start = 0, end = min(n, m);
+        int GCD = __gcd(n1,n2);
         
-        string ans = "";
-        string current = "";
-        
-        while(start < end) {
-            current += str1[start];
-            if(check(str1, str2, current)) {
-                ans = current;
-            }
-            ++start;
-        }
-        
-        return ans;
-    }
-    
-    bool check(string &s1, string &s2, string t) {
-        int n = s1.size();
-        int m = s2.size();
-        int k = t.size();
-        
-        if(n % k != 0 || m % k != 0) return false;
-        
-        for(int i = 0; i < n; ++i) {
-            if(s1[i] != t[i%k]) return false;
-        }
-        
-        for(int i = 0; i < m; ++i) {
-            if(s2[i] != t[i%k]) return false;
-        }
-        
-        return true;
+        return str1+str2 == str2+str1 ? str1.substr(0, GCD) : "";
     }
 };
