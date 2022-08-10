c2dbd13ae6b6d2675198a57a527db7e7a1a31bf4
Angel Tabares <taho2509@gmail.com>
2022-08-09 22:34:04 -0400

    Time: 44 ms (35.05%), Space: 6.7 MB (41.76%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,38 +1,19 @@
 class Solution {
-    vector<bool> isprime;
 public:
-    int minSteps(int n) {
-        if(n == 1) return 0;
-        cribe();
-        
-        if(isprime[n]) return n;
-        
+    int minSteps(int n) {        
         vector<int> DP(n + 1, 0);
         DP[0] = 0;
         DP[1] = 0;
         
         for(int i = 2; i <= n; ++i) {
             DP[i] = i;
-            if(!isprime[i]) {
-                for(int j = 2; j < i; ++j) {
-                    if(i%j == 0) {
-                        DP[i] = min(DP[i], (i - j)/j + DP[j] + 1);
-                    }
+            for(int j = 2; j < i; ++j) {
+                if(i%j == 0) {
+                    DP[i] = min(DP[i], (i - j)/j + DP[j] + 1);
                 }
             }
         }
         
         return DP[n];
-    }
-    
-    void cribe() {
-        isprime = vector<bool>(1001, true);
-        
-        for(int i = 2; i <= 1000; ++i) {
-            if(!isprime[i]) continue;
-            for(int j = 2*i; j <= 1000; j+=i) {
-                isprime[j] = false;
-            } 
-        }
     }
 };
