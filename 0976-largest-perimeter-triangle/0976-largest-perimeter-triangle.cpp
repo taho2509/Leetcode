3dd5105754089f37b9b0ce56e09136da7915b3f1
Angel Tabares <taho2509@gmail.com>
2022-10-12 00:29:22 -0300

    Time: 110 ms (5.51%), Space: 21.9 MB (22.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -5,8 +5,6 @@ public:
 
         int n = nums.size();
         for(int i = 2; i < n; ++i) {
-            if(nums[i - 2] + nums[i - 1] <= nums[i]) continue;
-            if(nums[i - 2] + nums[i] <= nums[i - 1]) continue;
             if(nums[i] + nums[i - 1] <= nums[i - 2]) continue;
             
             return nums[i - 2] + nums[i - 1] + nums[i];
