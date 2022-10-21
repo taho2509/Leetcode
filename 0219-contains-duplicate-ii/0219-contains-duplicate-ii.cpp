449d7001545525f46c2f1b13ebedbbf29079ca3d
Angel Tabares <taho2509@gmail.com>
2022-10-20 22:36:43 -0300

    Time: 473 ms (18.92%), Space: 80.5 MB (15.96%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        set<int> found;
+        int n = nums.size();
+        ++k;
+        
+        if(k > n) k = n;
+        
+        for(int i = 0; i < k; ++i) {
+            if(found.find(nums[i]) != found.end()) return true;
+            found.insert(nums[i]);
+        }
+        
+        for(int i = k; i < n; ++i) {
+            found.erase(nums[i - k]);
+            
+            if(found.find(nums[i]) != found.end()) return true;
+            found.insert(nums[i]);
+        }
+        
+        return false;
+    }
+};
