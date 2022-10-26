07713320cdb50212bd8084f08afc1c2a879adaaf
Angel Tabares <taho2509@gmail.com>
2022-10-25 23:58:42 -0300

    Time: 471 ms (50.95%), Space: 113.7 MB (71.68%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -7,6 +7,9 @@ public:
         
         for (int i = 0; i < n; ++i) {
             sum += nums[i];
+            // presums substractions
+            // a - b = n*k => (a - b)%k = n*k%k => a % k - b % k = 0
+            // a = b
             if (sums.count(sum % k)) return true;
             sums.insert(pre);
             pre = sum % k;
