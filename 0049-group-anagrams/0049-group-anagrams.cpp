491ad2f560929882732c7669ae13b5ebb0152dd7
Angel Tabares <taho2509@gmail.com>
2022-10-27 21:30:10 -0300

    Time: 70 ms (60.40%), Space: 20.7 MB (56.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> groups;
+       unordered_map<string, vector<string>> groups;
         for(string s: strs) {
             string ss = s;
             sort(ss.begin(), ss.end());
