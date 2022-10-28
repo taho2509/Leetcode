7cd1b5454082c67ba8d3f8c355157cffbb5503a3
Angel Tabares <taho2509@gmail.com>
2022-10-27 21:29:29 -0300

    Time: 98 ms (24.56%), Space: 20.9 MB (34.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<vector<int>, vector<string>> groups;
+        map<string, vector<string>> groups;
         for(string s: strs) {
-            groups[hash(s)].push_back(s);
+            string ss = s;
+            sort(ss.begin(), ss.end());
+            groups[ss].push_back(s);
         }
         
         vector<vector<string>> ans;
         for(auto it: groups) ans.push_back(it.second);
         return ans;
-    }
-    
-    vector<int> hash(string &s) {
-        vector<int> h(26, 0);
-        
-        for(char &a: s) h[a - 'a']++;
-        return h;
     }
 };
